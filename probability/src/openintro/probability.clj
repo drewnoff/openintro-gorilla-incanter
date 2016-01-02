f57d8db16ba369f62eb56584a5a8848c4c876667
drewnoff <drewnoff@yandex.com>
2016-01-02 21:17:44 +0300

    Probability Lab init

---
+++ probability/src/openintro/probability.clj
@@ -0,0 +1,25 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Probability
+;;; 
+;;; Original lab is available [here](http://htmlpreview.github.io/?https://github.com/andrewpbray/oiLabs-base-R/blob/master/probability/probability.html).
+;;; You can download the data for this lab from [here](https://www.openintro.org/stat/data/?data=kobe). 
+;; **
+
+;; @@
+(ns openintro.probability)
+
+(require '[incanter.core :as i]
+         '[incanter.charts :as c]
+         '[incanter.io :as iio]
+         '[incanter.stats :as s])
+(use 'incanter-gorilla.render :reload)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; @@
+
+;; @@
