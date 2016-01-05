6c539473bb65870471b020e2aa7f78bd12dbf038
drewnoff <drewnoff@yandex.com>
2016-01-05 15:22:07 +0400

    added 'Exploratory analysis' to inf-for-num-data lab

--- inf-for-numerical-data/src/openintro/inf-for-numerical-data.clj
+++ inf-for-numerical-data/src/openintro/inf-for-numerical-data.clj
@@ -69,6 +69,82 @@
 ;;; *1.  What are the cases in this data set? How many cases are there in our sample?*
 ;; **
 
+;; **
+;;; As a first step in the analysis, we should consider summaries of the data. For categorical columns this
+;;; can be done using the `category-col-summarizer` function and for numerical columns we can use `quantile` function:
+;; **
+
+;; @@
+(doall
+  (map println
+       (map #(s/category-col-summarizer % nc)
+            [:mature :premie :marital :gender :habit :whitemom])))
+
+(println (clojure.string/join "\t" ["col" "Min." "1st Qu." "Median" "3rd Qu" "Max."]))
+(doall
+  (map #(println (clojure.string/join "\t" %))
+       (map #(conj (s/quantile (filter (partial not= "NA") (i/sel nc :cols %))) %)
+            [:fage :mage :weeks :visits :gained :weight])))
+;; @@
+;; ->
+;;; {:col :mature, :count 0, :is-numeric false, younger mom 867, mature mom 133}
+;;; {:col :premie, :count 0, :is-numeric false, full term 846, premie 152, NA 2}
+;;; {:col :marital, :count 0, :is-numeric false, not married 613, married 386, NA 1}
+;;; {:col :gender, :count 0, :is-numeric false, female 503, male 497}
+;;; {:col :habit, :count 0, :is-numeric false, nonsmoker 873, smoker 126, NA 1}
+;;; {:col :whitemom, :count 0, :is-numeric false, white 714, not white 284, NA 2}
+;;; col	Min.	1st Qu.	Median	3rd Qu	Max.
+;;; :fage	14.0	25.0	30.0	35.0	55.0
+;;; :mage	13.0	22.0	27.0	32.0	50.0
+;;; :weeks	20.0	37.0	39.0	40.0	45.0
+;;; :visits	0.0	10.0	12.0	15.0	30.0
+;;; :gained	0.0	20.0	30.0	38.0	85.0
+;;; :weight	1.0	6.38	7.31	8.06	11.75
+;;; 
+;; <-
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}],"value":"(nil nil nil nil nil nil)"}
+;; <=
+
+;; **
+;;; For numerical variables, are there outliers? If you 
+;;; aren't sure or want to take a closer look at the data, make a graph.
+;;; 
+;;; Consider the possible relationship between a mother's smoking habit and the 
+;;; weight of her baby. Plotting the data is a useful first step because it helps 
+;;; us quickly visualize trends, identify strong associations, and develop research
+;;; questions.
+;;; 
+;;; *2.  Make a side-by-side boxplot of `habit` and `weight`. What does the plot 
+;;; highlight about the relationship between these two variables?*
+;;; 
+;;; The box plots show how the medians of the two distributions compare, but we can
+;;; also compare the means of the distributions using the following function to 
+;;; split the `weight` variable into the `habit` groups, then take the mean of each
+;;; using the `mean` function.
+;; **
+
+;; @@
+(print (i/$rollup s/mean :weight [:habit] nc))
+;; @@
+;; ->
+;;; 
+;;; |    :habit |           :weight |
+;;; |-----------+-------------------|
+;;; | nonsmoker | 7.144272623138631 |
+;;; |    smoker | 6.828730158730158 |
+;;; |        NA |              3.63 |
+;;; 
+;; <-
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; **
+;;; There is an observed difference, but is this difference statistically 
+;;; significant? In order to answer this question we will conduct a hypothesis test.
+;; **
+
 ;; @@
 
 ;; @@
