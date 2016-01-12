c78770a62695b7c73030c0110852ae04574fd716
drewnoff <drewnoff@yandex.com>
2016-01-12 23:28:17 +0300

    init lab

---
+++ inf-for-categorical-data/src/openintro/inf-for-categorical-data.clj
@@ -0,0 +1,26 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Probability
+;;; 
+;;; Original lab is available [here](http://htmlpreview.github.io/?https://github.com/andrewpbray/oiLabs-base-R/blob/master/inf_for_categorical_data/inf_for_categorical_data.html).
+;;; You can download the data for this lab from [here](https://www.openintro.org/stat/data/?data=atheism). 
+;; **
+
+;; @@
+(ns openintro.inf-for-categorical-data)
+
+(require '[incanter.core :as i]
+         '[incanter.charts :as c]
+         '[incanter.io :as iio]
+         '[incanter.stats :as s]
+         '[incanter.distributions :as dist])
+(use 'incanter-gorilla.render :reload)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; **
+;;; ## Getting Started
+;; **
